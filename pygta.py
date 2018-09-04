aa299c1bbbea6df4f3c23eec528664b5cce23402
yash upadhyay <yashup1997@gmail.com>
2018-09-05 00:39:39 +0530

    edge detection

--- pygta.py
+++ pygta.py
@@ -5,17 +5,24 @@ import cv2
 import time
 from directkeys import ReleaseKey, PressKey,W,A,S,D
 
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img =cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
 
 last_time = time.time()
 
 while(True):
-    printscreen_pil = ImageGrab.grab(bbox=(0,40,1000,800))
-    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')
+    screen = np.array(ImageGrab.grab(bbox=(0,40,900,700)))
+    new_screen = process_img(screen)
+    #printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')
     print('loop took {} seconds '.format(time.time()-last_time))
     
     last_time = time.time()
-    cv2.imshow('window',np.array(printscreen_pil))
+    cv2.imshow('window',new_screen)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+    
     
