e89cb0dbb67e1556911accd0f2e1ccdaf14b24e3
yash upadhyay <yashup1997@gmail.com>
2018-09-05 03:18:27 +0530

    lane detection

--- pygta.py
+++ pygta.py
@@ -13,13 +13,23 @@ def roi(img,vertices):
     return masked
 
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img,(coords[0],coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+    except:
+        pass
 
 def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img =cv2.Canny(processed_img, threshold1=200, threshold2=300)
-    
+    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
     vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
-    processed_img = roi(processed_img,[vertices])
+    #processed_img = roi(processed_img,[vertices])
+    
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,20,15)
+    draw_lines(processed_img, lines)
     
     return processed_img
 #for i in list(range(4))[::-1]:
